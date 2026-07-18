ee5128c73f8a4a35d4d0e48b66769cf1327fed8f
Amit <amitprajapati4353@gmail.com>
2026-07-18 21:42:05 +0530

    feat(usePathname): implement active navigation

---
+++ 15_usePathname/practice/app/_compnents/Footer.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <div className="p-5 flex gap-10 justify-center bg-zinc-500 text-xl font-semibold">
+        <p>all copy right reserved</p>
+    </div>
+  )
+}
---
+++ 15_usePathname/practice/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+   <div className="h-[80vh] font-bold flex justify-center items-center text-5xl">
+    Home Page
+   </div>
+  );
+}
