c925d56ffaf28e8c0d70ff669141717e2c3be73e
Amit <amitprajapati4353@gmail.com>
2026-08-09 22:48:08 +0530

    feat(layouts): implement shared layouts in Next.js

---
+++ 08_layouts/practice/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='w-full flex justify-center items-center'>
+      <p>Code by Amit Prajapati ❤️</p>
+    </div>
+  )
+}
